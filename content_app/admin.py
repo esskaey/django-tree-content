708cd71ad4233c359aba532ade55f582380b6d64
sk@sig <59538059+esskaey@users.noreply.github.com>
2023-09-22 08:44:39 +0000

    first commit

--- content_app/admin.py
+++ content_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import ContentModel, ExceptionContentModel
 # Register your models here.
+admin.site.register(ContentModel,ExceptionContentModel)
--- content_app/models.py
+++ content_app/models.py
@@ -1,5 +1,6 @@
 from ambient_toolbox.models import CommonInfo
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from tree_node.models import Node, TreeNodeType
@@ -11,8 +12,22 @@ class ContentModel(CommonInfo):
     node = models.ForeignKey(
         Node,
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
         limit_choices_to={"object_type": TreeNodeType.EXCEPTION},
     )
 
     class Meta:
         abstract = True
+
+
+class ExceptionContentModel(ContentModel):
+    description = models.TextField(help_text="Enter some description on the exception")
+    operator_text = models.TextField(help_text="Enter some operator text here")
+    service_text = models.TextField(help_text="Enter some service text here")
+
+    def clean(self):
+        if self.node.object_type != TreeNodeType.EXCEPTION:
+            raise ValidationError(
+                "Cannot link content model to a node other than exception type"
+            )
